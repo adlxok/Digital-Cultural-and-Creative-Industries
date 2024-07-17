b73ac211b10b86b9c6df618ae3796f957ca3b659
huang minxia <18178071350@163.com>
2024-07-17 17:23:51 +0800

    项目修改

--- DigitalCultural/DigitalCultural-portal/src/main/java/org/example/digitalculturalportal/pojo/CommunityPost.java
+++ DigitalCultural/DigitalCultural-portal/src/main/java/org/example/digitalculturalportal/pojo/CommunityPost.java
@@ -21,27 +21,51 @@ public class CommunityPost {
     @Id
     private int id;
 
+    /**
+     * 用户id
+     */
     @Field(type = FieldType.Integer)
     private int userId;
 
+    /**
+     * 标题
+     */
     @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_smart")
     private String title;
 
+    /**
+     * 内容
+     */
     @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_smart")
     private String content;
 
+    /**
+     * 帖子类型 0-普通;1-置顶;
+     */
     @Field(type = FieldType.Integer)
     private int type;
 
+    /**
+     * 评论状态 0-正常;1-精华;2-拉黑;
+     */
     @Field(type = FieldType.Integer)
     private int status;
 
+    /**
+     * 发布时间
+     */
     @Field(type = FieldType.Date)
     private Date createTime;
 
+    /**
+     * 评论数量
+     */
     @Field(type = FieldType.Integer)
     private int commentCount;
 
+    /**
+     * 帖子分数
+     */
     @Field(type = FieldType.Double)
     private double score;
 }
