0572beec47bcbd7c7d1d702e65c633deecac90de
huang minxia <18178071350@163.com>
2024-07-30 10:41:18 +0800

    评论&消息表

--- DigitalCultural/DigitalCultural-portal/document/sql/dcc.sql
+++ DigitalCultural/DigitalCultural-portal/document/sql/dcc.sql
@@ -155,5 +155,43 @@ CREATE TABLE `community_post` (
                                   PRIMARY KEY (`id`),
                                   KEY `index_user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- --------------------------------
+-- Table structure for community_comment
+-- --------------------------------
+DROP TABLE IF EXISTS `community_comment`;
+SET character_set_client = utf8mb4 ;
+CREATE TABLE `community_comment` (
+                                     `id` int(11) NOT NULL AUTO_INCREMENT,
+                                     `user_id` int(11) DEFAULT NULL,
+                                     `entity_type` int(11) DEFAULT NULL,
+                                     `entity_id` int(11) DEFAULT NULL,
+                                     `target_id` int(11) DEFAULT NULL,
+                                     `content` text,
+                                     `status` int(11) DEFAULT NULL,
+                                     `create_time` timestamp NULL DEFAULT NULL,
+                                     PRIMARY KEY (`id`),
+                                     KEY `index_user_id` (`user_id`),
+                                     KEY `index_entity_id` (`entity_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- --------------------------------
+-- Table structure for community_message
+-- --------------------------------
+
+DROP TABLE IF EXISTS `community_message`;
+SET character_set_client = utf8mb4 ;
+CREATE TABLE `community_message` (
+                                     `id` int(11) NOT NULL AUTO_INCREMENT,
+                                     `from_id` int(11) DEFAULT NULL,
+                                     `to_id` int(11) DEFAULT NULL,
+                                     `conversation_id` varchar(45) NOT NULL,
+                                     `content` text,
+                                     `status` int(11) DEFAULT NULL COMMENT '0-未读;1-已读;2-删除;',
+                                     `create_time` timestamp NULL DEFAULT NULL,
+                                     PRIMARY KEY (`id`),
+                                     KEY `index_from_id` (`from_id`),
+                                     KEY `index_to_id` (`to_id`),
+                                     KEY `index_conversation_id` (`conversation_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 
