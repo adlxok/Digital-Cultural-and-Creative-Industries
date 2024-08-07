ae803b23bce1949cf78b9ebccde43fd714ee36bb
huang minxia <18178071350@163.com>
2024-08-07 12:13:45 +0800

    社区帖子功能修改1

--- DigitalCultural/DigitalCultural-portal/src/main/java/org/example/digitalculturalportal/controller/CommunityPostController.java
+++ DigitalCultural/DigitalCultural-portal/src/main/java/org/example/digitalculturalportal/controller/CommunityPostController.java
@@ -6,8 +6,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.example.digitalculturalportal.common.CommonResult;
 import org.example.digitalculturalportal.common.ResultCode;
+import org.example.digitalculturalportal.pojo.CommunityEvent;
 import org.example.digitalculturalportal.pojo.CommunityPost;
 import org.example.digitalculturalportal.pojo.LoginUser;
+import org.example.digitalculturalportal.rabbitmqevent.CommunityProducer;
 import org.example.digitalculturalportal.service.CommunityPostService;
 import org.example.digitalculturalportal.utils.CommunityConstant;
 import org.example.digitalculturalportal.utils.RedisCache;
@@ -19,6 +21,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 社区帖子Controller
@@ -33,6 +37,8 @@ import java.util.Date;
 public class CommunityPostController implements CommunityConstant {
     @Autowired
     private CommunityPostService communityPostService;
+    @Autowired
+    private CommunityProducer communityProducer;
     @Autowired
     private RedisCache redisCache;
 
@@ -54,6 +60,7 @@ public class CommunityPostController implements CommunityConstant {
         communityPost.setUserId(Math.toIntExact(userId));
         communityPost.setCreateTime(new Date());
         int post=communityPostService.addCommunityPost(communityPost);
+
         //帖子存进redis定时计算更新分数
         if(communityPost.getId()==0) {
             log.error("获取id失败：加分失败");
@@ -61,6 +68,17 @@ public class CommunityPostController implements CommunityConstant {
             String key=RedisKeyUtil.getScoreKey();
             redisCache.setCacheObjectInSet(key, communityPost.getId());
         }
+        //向消息队列发送消息
+        Map<String,Object> map=new HashMap<>();
+        CommunityEvent communityEvent=new CommunityEvent();
+        communityEvent.setEventType(EVENT_TYPE_ADDPOST);
+        communityEvent.setEntityId(communityPost.getId());
+        communityEvent.setEntityType(ENTITY_TYPE_POST);
+        communityEvent.setUserId(communityPost.getUserId());
+        communityEvent.setEntityUserId(communityPost.getUserId() );
+        map.put("postId",communityPost.getId());
+        communityEvent.setData(map);
+        communityProducer.sendAddPostEvent(communityEvent);
         return CommonResult.success("发帖成功");
     }
     @ApiOperation("置顶帖子")
@@ -84,10 +102,22 @@ public class CommunityPostController implements CommunityConstant {
     @RequestMapping(value = "/deletePost", method = RequestMethod.GET)
     @ResponseBody
     public CommonResult deletePost(@RequestParam("id") Integer id){
+        CommunityPost communityPost=communityPostService.queryCommunityPostById(id);
         communityPostService.alterCommentStatus(id,POST_STATUS_BLACK);
         //更新缓存，重新加载数据
         communityPostService.deleteCache();
         communityPostService.hostPostInCache();
+
+        //向消息队列发送消息
+        Map<String,Object> map=new HashMap<>();
+        CommunityEvent communityEvent=new CommunityEvent();
+        communityEvent.setEventType(EVENT_TYPE_ADDPOST);
+        communityEvent.setEntityId(communityPost.getId());
+        communityEvent.setEntityType(ENTITY_TYPE_POST);
+        communityEvent.setUserId(communityPost.getUserId());
+        map.put("postId",communityPost.getId());
+        communityEvent.setData(map);
+        communityProducer.sendDeletePostEvent(communityEvent);
         return CommonResult.success("删帖成功");
     }
 
